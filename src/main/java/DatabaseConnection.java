0a9e5834f31d24f5ede72eb42e1cc8f9c95c6d40
kTurek95 <k.turek1995@gmail.com>
2024-06-09 20:39:26 +0100

    Updated DatabaseConnection file to use dotenv library for configuration

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,15 +1,17 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection
 {
-    private static final String URL = "";
+    static Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_URL");
 
-    private static final String USER = "";
+    private static final String USER = dotenv.get("DB_USER");
 
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection()
     {
@@ -18,12 +20,12 @@ public class DatabaseConnection
         try
         {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Połączenie uzyskane");
+            System.out.println("Connection obtained");
         }
         catch (SQLException e)
         {
             e.printStackTrace();
-            System.out.println("Nie udało się połączyć z bazą danych");
+            System.out.println("Failed to connect to the database");
         }
 
         return connection;
